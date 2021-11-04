734897a46fe212b943a554114b3a749dbe51f875
mtrw <mtrw@gmail.com>
2021-11-04 13:25:37 +0100

    minor bug fix again

--- tim_functions.R
+++ tim_functions.R
@@ -433,8 +433,10 @@ null_plot <- function(x,y,xlab=NA,ylab=NA,revx=F,revy=F,...){
   yl<-range(y,na.rm=T)
   if(revx==T){ xl <- rev(xl) }
   if(revy==T){ yl <- rev(yl) }
-  plot(NULL,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,...)
+  plot(NULL,xlim=xl,ylim=yl,xlab=xlab,ylab=ylab,...)
 }
+
+
 
 
 #turn levels of a factor into colours from a colorspace palette (in the diverge_hcl set)
